e99cc1977d85d452709c80ec6d35bb76ca24bafe
mosterbox <romapotapov7@gmail.com>
2019-07-24 09:29:13 +0300

    added function to like posts

--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -1,22 +1,26 @@
 // Core
 import React, { Component } from 'react';
 import moment from 'moment';
-import PropTypes from 'prop-types';
+import { func, string, number, array } from 'prop-types';
 
 // Instruments
 import Styles from './styles.m.css';
 
 // Components
+import Like from 'components/Like';
 import { Consumer } from 'components/HOC/withProfile';
 
 export default class Post extends Component {
     static propTypes = {
-        comment: PropTypes.string.isRequired,
-        created: PropTypes.number.isRequired,
+        _likePost: func.isRequired,
+        comment:   string.isRequired,
+        created:   number.isRequired,
+        likes:     array.isRequired,
+        id:        string.isRequired,
     };
 
     render () {
-        const { comment, created } = this.props;
+        const { comment, created, _likePost, id, likes } = this.props;
 
         return (
             <Consumer>
@@ -26,6 +30,12 @@ export default class Post extends Component {
                         <a>{`${context.currentUserFirstName} ${context.currentUserLastName}`}</a>
                         <time>{moment.unix(created).format('MMMM D h:mm:ss a')}</time>
                         <p>{ comment }</p>
+                        <Like
+                            _likePost = { _likePost }
+                            id = { id }
+                            likes = { likes }
+                            { ...context }
+                        />
                     </section>
                 )}
             </Consumer>
